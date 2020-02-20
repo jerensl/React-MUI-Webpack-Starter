a8211fa8f14ac0057abb26ddeb269bc1f28763f7
Jerens S. Lensun <54782057+jerensl22@users.noreply.github.com>
2020-02-20 09:48:05 +0800

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const modeConfig = env => require(`./config/webpack.${env}`)(env)
 const loadPresets = require("./config/loadPresets")
 
-module.exports = ({ mode = "production", presets = [] }) => {
+module.exports = (
+  { mode: mode = "production", presets: presets = [] } = { mode, presets },
+) => {
   return merge(
     {
       mode,
@@ -23,11 +25,52 @@ module.exports = ({ mode = "production", presets = [] }) => {
             use: ["babel-loader", "eslint-loader"],
           },
           {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"],
+            test: /\.module\.css$/i,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: {
+                  modules: false,
+                },
+              },
+            ],
           },
           {
             test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  outputPath: "./assets/images",
+                },
+              },
+              {
+                loader: "image-webpack-loader",
+                options: {
+                  mozjpeg: {
+                    progressive: true,
+                    quality: 65,
+                  },
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: [0.65, 0.9],
+                    speed: 4,
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  webp: {
+                    quality: 75,
+                  },
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
             use: ["file-loader"],
           },
         ],
