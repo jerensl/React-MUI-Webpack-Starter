2c531f1ac38a9988216623a5f41eb604b842e9d4
jerensl22 <jerenslensun@gmail.com>
2020-03-22 23:26:21 +0800

    'upgrade'

--- config/presets/webpack.compres.js
+++ config/presets/webpack.compres.js
@@ -1,5 +1,15 @@
-const compress = require("compression-webpack-plugin");
+const compress = require("compression-webpack-plugin")
 
 module.exports = () => ({
-  plugins: [new compress()]
-});
+  plugins: [
+    new compress({
+      filename: "[path].br[query]",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    }),
+  ],
+})
